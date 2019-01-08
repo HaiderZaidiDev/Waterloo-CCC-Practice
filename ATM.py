a64b8eae46b4a4af7f4fc3d0236e393381fa205e
HaiderZaidiDev <45272003+HaiderZaidiDev@users.noreply.github.com>
2019-01-07 22:07:13 -0500

    Completed ATM.py

--- ATM.py
+++ ATM.py
@@ -1,3 +1,16 @@
 #--- CodeChef ATM Beginner Prompt
 # https://www.codechef.com/problems/HS08TEST
 
+atmInfo = input().split()
+
+withdrawAMNT = float(atmInfo[0])
+accBalance = float(atmInfo[1])
+
+if withdrawAMNT % 5 != 0:
+  print(accBalance) 
+elif withdrawAMNT > accBalance - 0.5:
+  print(accBalance)
+
+else:
+  print(accBalance - withdrawAMNT - 0.5)
+  
